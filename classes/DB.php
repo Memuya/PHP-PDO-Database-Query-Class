97806c73a13f9c12151211a860c30e895dba8139
Mehmet Uyanik <Memuya@users.noreply.github.com>
2015-07-10 13:26:51 +1000

    Create DB.php

---
+++ classes/DB.php
@@ -0,0 +1,53 @@
+<?php
+class DB {
+	protected static $connected = false;
+	protected $db;
+	protected $host = "localhost";
+	protected $dbname = "dbname";
+	protected $user = "root";
+	protected $pass = "";
+
+	/**
+	* Calls the connect() method to connect to the database when the DB object is created
+	*/
+	public function __construct() {
+		try {
+			$this->connect();
+		} catch(PDOException $ex) {
+			die($ex->getMessage());
+		}
+	}
+
+	/**
+	* Connects to database via PDO and sets the error mode to exception
+	*/
+	public function connect() {
+		$this->db = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname, $this->user, $this->pass);
+		$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+
+	/**
+	* Returns the database connection to be used outside of the clas
+	*
+	* e.g. $q = $this->db->getDB()->query("sql");
+	*/
+	public function getDB() {
+		return $this->db;
+	}
+
+	public function setDBName($name) {
+		$this->dbname = $name;
+	}
+
+	public function setHost($host) {
+		$this->host = $host;
+	}
+
+	public function setDBUser($user) {
+		$this->user = $user;
+	}
+
+	public function setDBPass($pass) {
+		$this->pass = $pass;
+	}
+}
