75ba1628125a4382c9e54f4edd310fa8de964a7f
Mehmet Uyanik <Memuya@users.noreply.github.com>
2015-07-10 13:27:23 +1000

    Create Query.php

---
+++ classes/Query.php
@@ -0,0 +1,115 @@
+<?php
+require_once 'classes/DB.php';
+class Query extends DB {
+	private $count;
+	private $data;
+	private $sql;
+	private $values;
+	private $fetch_option;
+
+	/**
+	* Set sql query and the values to bind to the query
+	*
+	* @param string $sql
+	* @param array $values
+	*/
+	public function __construct($sql, $values = []) {
+		parent::__construct();
+		$this->sql = $sql;
+		$this->values = $values;
+		$this->fetch_option = PDO::FETCH_OBJ;
+	}
+
+	/**
+	* Returns all rows from the database
+	*/
+	public function getAll($fetch_option = PDO::FETCH_OBJ) {
+		$this->fetch_option = (!empty($fetch_option)) ? $fetch_option : $this->fetch_option;
+
+		try {
+			$q = $this->db->prepare($this->sql);
+
+			$q->execute($this->values);
+		} catch(PDOException $ex) {
+			die($ex->getMessage());
+		}
+		
+		$this->count = $q->rowCount();
+		
+		if($this->count != 0)
+			while($r = $q->fetch($this->fetch_option)) 
+				$this->data[] = $r;
+				  
+		return $this->data;
+	}
+
+	/**
+	* Returns a single row from the database
+	* Use as $obj->single()->fieldYouWant
+	*/
+	public function single($fetch_option = PDO::FETCH_OBJ) {
+		$this->fetch_option = (!empty($fetch_option)) ? $fetch_option : $this->fetch_option;
+
+		try {
+			$q = $this->db->prepare($this->sql);
+
+			$q->execute($this->values);
+		} catch(PDOException $ex) {
+			die($ex->getMessage());
+		}
+		
+		$this->count = $q->rowCount();
+		
+		if($this->count != 0)
+			$this->data = $q->fetch($this->fetch_option);
+				  
+		return $this->data;
+	}
+
+	/**
+	* Executes a query and return the amount of rows affected if no message is passed through
+	* Used for queries such as DELETE, INSERT, UPDATE, etc
+	*/
+	public function commit($message = null) {
+		try {
+			$q = $this->db->prepare($this->sql);
+
+			$q->execute($this->values);
+		} catch(PDOException $ex) {
+			die($ex->getMessage());
+		}
+
+		$this->count = $q->rowCount();
+		$suffix = ($this->count != 1) ? 's' : null;
+
+		return (!empty($message)) ? $message : $this->count." row".$suffix." affected";
+	}
+
+	/**
+	* Returns the last inserted ID
+	*/
+	public function lastInsertId() {
+		return $this->db->lastInsertId();
+	}
+
+	/**
+	* Returns the amount of rows affected from a query
+	*/
+	public function getCount() {
+		return $this->count;
+	}
+
+	/**
+	* Returns the data array
+	*/
+	public function getData() {
+		return $this->data;
+	}
+
+	/**
+	* Returns the SQL query as a string
+	*/
+	public function getSQL() {
+		return $this->sql;
+	}
+}
